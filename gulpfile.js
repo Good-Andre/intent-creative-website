eaa13fbf5122603acec5f53987c0550d161ddd72
good_andre <good-andre@yandex.ru>
2021-02-09 17:11:47 +0300

    add portfolio and js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const {src, dest, parallel, series, watch} = require('gulp');
+const { src, dest, parallel, series, watch } = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify-es').default;
@@ -9,36 +9,29 @@ const rename = require('gulp-rename');
 const fileinclude = require('gulp-file-include');
 const sourcemaps = require('gulp-sourcemaps');
 const notify = require('gulp-notify');
-const webpack = require('webpack');
-const webpackStream = require('webpack-stream');
-const ttf2woff2 = require('gulp-ttf2woff2');
-const fs = require('fs');
-const imagemin = require('gulp-imagemin');
-const webp = require('gulp-webp');
-const imageminPngquant = require('imagemin-pngquant');
-const imageminZopfli = require('imagemin-zopfli');
-const imageminMozjpeg = require('imagemin-mozjpeg');
-const imageminGiflossy = require('imagemin-giflossy');
-const imageminWebp = require('imagemin-webp');
+const image = require('gulp-image');
 const htmlmin = require('gulp-htmlmin');
 const groupMedia = require('gulp-group-css-media-queries');
+const concat = require('gulp-concat');
+const gulpif = require('gulp-if');
+let isProd = false;
 
 const resources = () => {
   return src('./src/resources/**')
     .pipe(dest('./app/resources'))
     .pipe(browserSync.stream());
-}
+};
 
-const imgToApp = () => {
-  return src(['./src/img/**/*.{svg,jpg,JPG,jpeg,png,gif,ico,tiff}'])
-    .pipe(dest('./app/img'))
+const fonts = () => {
+  return src('./src/fonts/**')
+    .pipe(dest('./app/fonts/'))
     .pipe(browserSync.stream());
 };
 
-const webpToApp = () => {
-  return src(['./src/img/webp/*.{jpg,JPG,jpeg,png,webp}'])
-    .pipe(webp())
-    .pipe(dest('./app/img/webp'))
+const img = () => {
+  return src(['./src/img/**/*.{svg,jpg,JPG,jpeg,png,gif,ico,tiff,webp}'])
+    .pipe(gulpif(isProd, image()))
+    .pipe(dest('./app/img'))
     .pipe(browserSync.stream());
 };
 
@@ -47,145 +40,51 @@ const htmlInclude = () => {
     .pipe(fileinclude())
     .pipe(dest('./app'))
     .pipe(browserSync.stream());
-}
-
- const otf2ttf = () => {
-  return src([source_folder + '/fonts/*.otf'])
-    .pipe(
-      fonter({
-        formats: ['ttf'],
-      })
-    )
-    .pipe(dest(source_folder + '/fonts/'));
-}
-
-const fonts = () => {
-  return src('./src/fonts/**.ttf')
-    .pipe(ttf2woff2())
-    .pipe(dest('./app/fonts/'))
-    .pipe(browserSync.stream());
-}
-
-const checkWeight = (fontname) => {
-  let weight = 400;
-  switch (true) {
-    case /Thin/.test(fontname):
-      weight = 100;
-      break;
-    case /ExtraLight/.test(fontname):
-      weight = 200;
-      break;
-    case /Light/.test(fontname):
-      weight = 300;
-      break;
-    case /Regular/.test(fontname):
-      weight = 400;
-      break;
-    case /Medium/.test(fontname):
-      weight = 500;
-      break;
-    case /SemiBold/.test(fontname):
-      weight = 600;
-      break;
-    case /Semi/.test(fontname):
-      weight = 600;
-      break;
-    case /Bold/.test(fontname):
-      weight = 700;
-      break;
-    case /ExtraBold/.test(fontname):
-      weight = 800;
-      break;
-    case /Heavy/.test(fontname):
-      weight = 700;
-      break;
-    case /Black/.test(fontname):
-      weight = 900;
-      break;
-    default:
-      weight = 400;
-  }
-  return weight;
-}
-
-const cb = () => {}
-
-let srcFonts = './src/scss/_fonts.scss';
-let appFonts = './app/fonts/';
-
-const fontsStyle = (done) => {
-  let file_content = fs.readFileSync(srcFonts);
-
-  fs.writeFile(srcFonts, '', cb);
-  fs.readdir(appFonts, function (err, items) {
-    if (items) {
-      let c_fontname;
-      for (var i = 0; i < items.length; i++) {
-				let fontname = items[i].split('.');
-				fontname = fontname[0];
-        let font = fontname.split('-')[0];
-        let weight = checkWeight(fontname);
-
-        if (c_fontname != fontname) {
-          fs.appendFile(srcFonts, '@include font-face("' + font + '", "' + fontname + '", ' + weight +');\r\n', cb);
-        }
-        c_fontname = fontname;
-      }
-    }
-  })
-  done();
-}
+};
 
 const styles = () => {
   return src('./src/scss/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass({
-      outputStyle: 'expanded'
-    }).on("error", notify.onError()))
+    .pipe(
+      sass({
+        outputStyle: 'expanded',
+      }).on('error', notify.onError())
+    )
     .pipe(groupMedia())
-    .pipe(autoprefixer({
-      cascade: false,
-    }))
-    .pipe(cleanCSS({
-      level: 2
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(
+      cleanCSS({
+        level: 2,
+      })
+    )
+    .pipe(
+      rename({
+        suffix: '.min',
+      })
+    )
     .pipe(sourcemaps.write('.'))
     .pipe(dest('./app/css/'))
     .pipe(browserSync.stream());
-}
+};
 
 const scripts = () => {
-  return src('./src/js/main.js')
-    .pipe(webpackStream(
-      {
-        mode: 'development',
-        output: {
-          filename: 'main.js',
-        },
-        module: {
-          rules: [{
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
-          }]
-        },
-      }
-    ))
-    .on('error', function (err) {
-      console.error('WEBPACK ERROR', err);
-      this.emit('end');
-    })
-    .pipe(sourcemaps.init())
-    .pipe(uglify().on("error", notify.onError()))
-    .pipe(sourcemaps.write('.'))
+  src('./src/js/vendor/**.js')
+    .pipe(concat('vendor.js'))
+    .pipe(gulpif(isProd, uglify().on('error', notify.onError())))
+    .pipe(dest('./app/js/'));
+  return src([
+    './src/js/functions/**.js',
+    './src/js/components/**.js',
+    './src/js/main.js',
+  ])
+    .pipe(gulpif(!isProd, sourcemaps.init()))
+    .pipe(concat('main.js'))
+    .pipe(gulpif(isProd, uglify().on('error', notify.onError())))
+    .pipe(gulpif(!isProd, sourcemaps.write('.')))
     .pipe(dest('./app/js'))
     .pipe(browserSync.stream());
 }
@@ -193,9 +92,9 @@ const scripts = () => {
 const watchFiles = () => {
   browserSync.init({
     server: {
-      baseDir: "./app"
+      baseDir: './app',
     },
-    notify: false
+    notify: false,
   });
 
   watch('./src/scss/**/*.scss', styles);
@@ -203,166 +102,68 @@ const watchFiles = () => {
   watch('./src/html/*.html', htmlInclude);
   watch('./src/*.html', htmlInclude);
   watch('./src/resources/**', resources);
-  watch('./src/img/**/*.{svg,jpg,JPG,jpeg,png,gif,ico,tiff}', imgToApp);
-  watch('./src/img/**/*.{jpg,JPG,jpeg,png,webp}', webpToApp);
+  watch('./src/img/**/*.{svg,jpg,JPG,jpeg,png,gif,ico,tiff,webp}', img);
   watch('./src/fonts/**', fonts);
-}
+};
 
 const clean = () => {
-	return del(['app/*'])
-}
+  return del(['app/*']);
+};
 
 exports.fileinclude = htmlInclude;
-exports.styles = styles;
-exports.scripts = scripts;
-exports.watchFiles = watchFiles;
-exports.fonts = fonts;
-exports.fontsStyle = fontsStyle;
 
 exports.default = series(
   clean,
-  parallel(htmlInclude, scripts, fonts, 
-    resources, 
-    imgToApp, 
-    webpToApp
-    ),
+  parallel(htmlInclude, scripts, fonts, resources, img),
   styles,
   watchFiles
 );
 
-const imgToBuild = () => {
-  return src(['./src/img/**.{svg,jpg,JPG,jpeg,png,gif,ico,tiff}'])
-    .pipe(
-      imagemin([
-        imageminGiflossy({
-          optimizationLevel: 3,
-          optimize: 3,
-          lossy: 2,
-        }),
-        imageminPngquant({
-          speed: 8,
-          quality: [0.7, 0.9],
-        }),
-        imageminZopfli({
-          more: true,
-        }),
-        imageminMozjpeg({
-          progressive: true,
-          quality: 90,
-        }),
-        imagemin.svgo({
-          plugins: [
-            { removeViewBox: false },
-            { removeUnusedNS: false },
-            { removeUselessStrokeAndFill: false },
-            { cleanupIDs: false },
-            { removeComments: true },
-            { removeEmptyAttrs: true },
-            { removeEmptyText: true },
-            { collapseGroups: true },
-          ],
-        }),
-      ])
-    )
-    .pipe(dest('./app/img'));
-};
-
-const webpToBuild = () => {
-  return src(['./src/img/webp/**.{jpg,JPG,jpeg,png,webp}'])
-  .pipe(
-    webp(
-      imageminWebp({
-        lossless: true,
-        quality: 100,
-        alphaQuality: 100,
-      })
-    )
-  )
-  .pipe(dest('./app/img/webp'))
-}
-
 const stylesBuild = () => {
   return src('./src/scss/**/*.scss')
-    .pipe(sass({
-      outputStyle: 'expanded'
-    }).on("error", notify.onError()))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(autoprefixer({
-      cascade: false,
-    }))
-    .pipe(cleanCSS({
-      level: 2
-    }))
-    .pipe(dest('./app/css/'))
-}
-
-const scriptsBuild = () => {
-  return src('./src/js/main.js')
-    .pipe(webpackStream(
-        {
-          mode: 'production',
-          output: {
-            filename: 'main.js',
-          },
-          module: {
-            rules: [{
-              test: /\.m?js$/,
-              exclude: /(node_modules|bower_components)/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env']
-                }
-              }
-            }]
-          },
-        }))
-      .on('error', function (err) {
-        console.error('WEBPACK ERROR', err);
-        this.emit('end');
+    .pipe(
+      sass({
+        outputStyle: 'expanded',
+      }).on('error', notify.onError())
+    )
+    .pipe(
+      rename({
+        suffix: '.min',
       })
-    .pipe(uglify().on("error", notify.onError()))
-    .pipe(dest('./app/js'))
-}
+    )
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(
+      cleanCSS({
+        level: 2,
+      })
+    )
+    .pipe(dest('./app/css/'));
+};
 
-const cache = () => {
-  return src('app/**/*.{css,js,svg,png,jpg,jpeg,woff2}', {
-    base: 'app'})
-    .pipe(rev())
-    .pipe(revdel())
-    .pipe(dest('app'))
-    .pipe(rev.manifest('rev.json'))
+const htmlMinify = () => {
+  return src('app/**/*.html')
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
     .pipe(dest('app'));
 };
 
-const rewrite = () => {
-  const manifest = src('app/rev.json');
+const toProd = (done) => {
+  isProd = true;
+  done();
+};
 
-  return src('app/**/*.html')
-    .pipe(revRewrite({
-      manifest
-    }))
-    .pipe(dest('app'));
-}
-
-const htmlMinify = () => {
-	return src('app/**/*.html')
-		.pipe(htmlmin({
-			collapseWhitespace: true
-		}))
-		.pipe(dest('app'));
-}
-
-exports.cache = series(cache, rewrite);
-exports.imgToBuild = imgToBuild;
-
-exports.build = series(clean, parallel(htmlInclude, scriptsBuild, fonts,
-   resources,
-   imgToApp, 
-   webpToApp),
+exports.build = series(
+  toProd,
+  clean,
+  parallel(htmlInclude, scripts, fonts, resources),
+  stylesBuild,
   htmlMinify,
-  imgToBuild,
-  webpToBuild
+  img
 );
--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,164 @@
-const vars = {
-  $introSlider: document.querySelector('.home__slider'),
-  $mainSliderThumbs: document.querySelector('.home__slider-thumbs'),
-  $projectsFilter: document.querySelector('.projects__filter'),
-  $projectsItem: document.querySelectorAll('.projects__item'),
-  $projectsList: document.querySelector('.projects__list'),
-  $menuHeaderBurger: document.querySelector('.menu-header__burger'),
-  $menuHeaderNav: document.querySelector('.menu-header__nav'),
-  $headerTop: document.querySelector('.header__top'),
+const $navLink = document.querySelectorAll('.nav__link');
+const $introSlider = document.querySelector('.home__slider');
+const $mainSliderThumbs = document.querySelector('.home__slider-thumbs');
+const $projectsFilter = document.querySelector('.projects__filter');
+const $projectsItem = document.querySelectorAll('.projects__item');
+const $projectsList = document.querySelector('.projects__list');
+const $menuHeaderBurger = document.querySelector('.menu-header__burger');
+const $menuHeaderNav = document.querySelector('.menu-header__nav');
+const $headerTop = document.querySelector('.header__top');
+const $upBtn = document.querySelector('.up-btn');
+
+// SMALL SLIDER thumbs
+
+const galleryThumbsItem = new Swiper($mainSliderThumbs, {
+  spaceBetween: 1,
+  slidesPerView: 10,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  allowTouchMove: false
+});
+
+// MAIN SLIDER
+
+const introSliderItem = new Swiper($introSlider, {
+  loop: true,
+  slidesPerView: 1,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.home__slide-next',
+    prevEl: '.home__slide-prev',
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  speed: 400,
+  keyboard: {
+    enabled: true,
+  },
+  thumbs: {
+    swiper: galleryThumbsItem
+  },
+  pagination: {
+    el: '.home__slider-pagination',
+    clickable: true,
+  }
+});
+
+// PROJECTS FILTER mixitup lib.
+
+try {
+  var mixer = mixitup($projectsList, {
+    animation: {
+      effects: 'fade translateZ(-100px)'
+    },
+  });
+
+  // PROJECTS FILTER active btn
+
+  function filterFunc(e) {
+    if (e.target.classList.contains('projects__btn')) {
+      let catFilter = e.target.getAttribute('data-filter');
+      if (catFilter == 'all') {
+        if (!e.target.classList.contains('projects__btn--active')) {
+          this.querySelector('.projects__btn--active').classList.remove(
+            'projects__btn--active'
+          );
+          e.target.classList.add('projects__btn--active');
+        }
+      } else {
+        if (!e.target.classList.contains('projects__btn--active')) {
+          this.querySelector('.projects__btn--active').classList.remove(
+            'projects__btn--active'
+          );
+          e.target.classList.add('projects__btn--active');
+          $projectsItem.forEach((item) => {
+            if (item.dataset.cat != catFilter) {
+              item.classList.remove('projects__btn--active');
+            }
+          });
+        }
+      }
+    }
+  }
+
+  $projectsFilter.addEventListener('click', filterFunc, false);
+
+} catch (err) {
+  // console.log('Ошибка: ' + e.name);
 }
 
-const navLink = document.querySelectorAll('.nav__link');
-const gitUrl = "/intent-creative-website/app";
+// BURGER
 
-if (document.location.pathname === gitUrl + '/index.html' || document.location.pathname === gitUrl + '/') {
-  navLink.forEach(item =>{
-    if (item.pathname === gitUrl + '/index.html' || item.pathname === gitUrl + '/') {
-      item.classList.add('nav__link--active');
-    }
-  })
-} else if (document.location.pathname === gitUrl + '/blog.html') {
-  navLink.forEach(item =>{
-    if (item.pathname === gitUrl + '/blog.html') {
-      item.classList.add('nav__link--active');
-    }
-  })
-} else if (document.location.pathname === gitUrl + '/portfolio.html') {
-  navLink.forEach(item =>{
-    if (item.pathname === gitUrl + '/portfolio.html') {
-      item.classList.add('nav__link--active');
-    }
-  })
+$menuHeaderBurger.addEventListener('click', ()=> {
+  $menuHeaderBurger.classList.toggle('_active');
+  $menuHeaderNav.classList.toggle('_active');
+  $headerTop.classList.toggle('_active');
+});
 
-} else if (document.location.pathname === gitUrl + '/contact.html') {
-  navLink.forEach(item =>{
-    console.log(item.pathname);
-    if (item.pathname === gitUrl + '/contact.html') {
-      item.classList.add('nav__link--active');
+const activeDelete = () => {
+  $menuHeaderBurger.classList.remove('_active');
+  $menuHeaderNav.classList.remove('_active');
+  $headerTop.classList.remove('_active');
+}
+
+// resize plugin
+
+const resizeContent = (
+  selector,
+  mobileWidth = 576,
+  isPlugin,
+  desktopAction = null,
+  mobileAction = null
+) => {
+  if (!isPlugin) {
+    if (window.innerWidth > mobileWidth) {
+      desktopAction();
+    } else {
+      mobileAction();
     }
-  })
+  } else {
+    if (
+      window.innerWidth <= mobileWidth &&
+      selector.dataset.pluginActivated == 'false'
+    ) {
+      mobileAction();
+      selector.dataset.pluginActivated = 'true';
+    }
+    if (window.innerWidth > mobileWidth) {
+      desktopAction();
+      selector.dataset.pluginActivated = 'false';
+    }
+  }
 };
 
-import './components/main-slider';
-import './components/portfolio-filter';
+// RESIZE EVENTS
 
-vars.$menuHeaderBurger.addEventListener('click', ()=> {
-  vars.$menuHeaderBurger.classList.toggle('_active');
-  vars.$menuHeaderNav.classList.toggle('_active');
-  vars.$headerTop.classList.toggle('_active');
+window.addEventListener('resize', () => {
+  resizeContent(
+    $menuHeaderBurger,
+    480,
+    true,
+    activeDelete,
+    ()=>{}
+  );
+});
+
+// BTN-UP scroll to TOP
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 400) {
+    $upBtn.style.display = 'block';
+  } else {
+    $upBtn.style.display = 'none';
+  }
+});
+
+$upBtn.addEventListener('click', () => {
+  window.scroll({
+    behavior: 'smooth',
+    top: document.getElementById('top')
+  });
 });
